09168525e5735a6e349bc91fe40e40dedc43d3eb
zyneltech1-jpg <zyneltech1@gmail.com>
2026-03-26 13:24:35 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWmVyjSdRpXx_O6JMRzroK-Vf4gtLEuEo",
+    authDomain: "green-centre-store-ef1db.firebaseapp.com",
+    projectId: "green-centre-store-ef1db",
+    storageBucket: "green-centre-store-ef1db.firebasestorage.app",
+    messagingSenderId: "590493205066",
+    appId: "1:590493205066:web:851b0bb69c2449db319cb3",
+    measurementId: "G-SSCVJDYV3B"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+console.log("Firebase connected successfully 🔥");
